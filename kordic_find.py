0da07d81ecf462731aa7b5016fb7241291d1e8d7
nlp_kitak <gitagggao@naver.com>
2017-06-28 15:28:11 +0900

    test

--- kordic_find.py
+++ kordic_find.py
@@ -2,7 +2,7 @@ import pymongo
 connection = pymongo.MongoClient("203.255.81.137", 27017)
 kordicDB = connection.kordicDB
 data = kordicDB.data_2014
-word = '가'
+word = '가속도'
 list = data.find({"word": word})
 for l in list:
     print("word : %s, pos : %s, WSD : %s, 의미: %s" %(l['word'],l['pos'],l['meannum1'],l['mean']))
